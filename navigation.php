2444e0808c8160282ca5922c90d98546acbfb0d7
Nate Corkish <66919946+natecorkish@users.noreply.github.com>
2022-09-06 23:18:27 +0100

    No comment commit

--- navigation.php
+++ navigation.php
@@ -2,36 +2,36 @@
 
 return [
     'Getting Started' => [
-        'url' => 'docs/summary',
+        'url' => 'http://gopayee.com/cashier-usage-docs/docs/summary',
         'children' => [
-            'Installation' => 'docs/getting-started/installation',
-            'Preparing the model' => 'docs/getting-started/preparing-the-model',
-            'Preparing the service provider' => 'docs/getting-started/preparing-the-service-provider',
+            'Installation' => 'http://gopayee.com/cashier-usage-docs/docs/getting-started/installation',
+            'Preparing the model' => 'http://gopayee.com/cashier-usage-docs/docs/getting-started/preparing-the-model',
+            'Preparing the service provider' => 'http://gopayee.com/cashier-usage-docs/docs/getting-started/preparing-the-service-provider',
         ],
     ],
     'Plans' => [
-        'url' => 'docs/summary',
+        'url' => 'http://gopayee.com/cashier-usage-docs/docs/summary',
         'children' => [
-            'Defining the plans' => 'docs/defining-plans/defining-the-plans',
-            'Float and integer values' => 'docs/defining-plans/features/types-of-values',
-            'Resetting values on payment' => 'docs/defining-plans/resetting-values',
-            'Plan eligibility' => 'docs/defining-plans/checking-for-exceeded-quotas',
-            'Sync usage values' => 'docs/defining-plans/preventing-feature-usage-failures',
-            'Inherit features from other plans' => 'docs/defining-plans/inherit-features-from-other-plans',
-            'Metadata' => 'docs/defining-plans/additional-metadata',
+            'Defining the plans' => 'http://gopayee.com/cashier-usage-docs/docs/defining-plans/defining-the-plans',
+            'Float and integer values' => 'http://gopayee.com/cashier-usage-docs/docs/defining-plans/features/types-of-values',
+            'Resetting values on payment' => 'http://gopayee.com/cashier-usage-docs/docs/defining-plans/resetting-values',
+            'Plan eligibility' => 'http://gopayee.com/cashier-usage-docs/docs/defining-plans/checking-for-exceeded-quotas',
+            'Sync usage values' => 'http://gopayee.com/cashier-usage-docs/docs/defining-plans/preventing-feature-usage-failures',
+            'Inherit features from other plans' => 'http://gopayee.com/cashier-usage-docs/docs/defining-plans/inherit-features-from-other-plans',
+            'Metadata' => 'http://gopayee.com/cashier-usage-docs/docs/defining-plans/additional-metadata',
         ],
     ],
     'Stripe Features' => [
-        'url' => 'docs/summary',
+        'url' => 'http://gopayee.com/cashier-usage-docs/docs/summary',
         'children' => [
-            'Metered Features' => 'docs/stripe-features/stripe-metered-billing/metered-features',
-            'Mid-Exceed Quotas in Stripe Metered Billing' => 'docs/stripe-features/stripe-metered-billing/mid-exceed-quotas-in-stripe-metered-billing',
+            'Metered Features' => 'http://gopayee.com/cashier-usage-docs/docs/stripe-features/stripe-metered-billing/metered-features',
+            'Mid-Exceed Quotas in Stripe Metered Billing' => 'http://gopayee.com/cashier-usage-docs/docs/stripe-features/stripe-metered-billing/mid-exceed-quotas-in-stripe-metered-billing',
         ],
     ],
     'Static Items' => [
-        'url' => 'docs/summary',
+        'url' => 'http://gopayee.com/cashier-usage-docs/docs/summary',
         'children' => [
-            'Defining Items' => 'docs/static-items/defining-items',
+            'Defining Items' => 'http://gopayee.com/cashier-usage-docs/docs/static-items/defining-items',
         ],
     ],
 ];
