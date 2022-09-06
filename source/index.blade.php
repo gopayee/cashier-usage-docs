f2977d5505ea9566919afbcba8049ea41012902b
Nate Corkish <66919946+natecorkish@users.noreply.github.com>
2022-09-06 23:06:55 +0100

    No comment commit

--- source/index.blade.php
+++ source/index.blade.php
@@ -11,7 +11,7 @@
             <p class="text-lg">Easily manage usage quotas for cashier.</p>
 
             <div class="flex my-10">
-                <a href="/docs/summary" title="{{ $page->siteName }} getting started" class="bg-blue-500 hover:bg-blue-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">Get Started</a>
+                <a href="http://gopayee.com/cashier-usage-docs/docs/summary" title="{{ $page->siteName }} getting started" class="bg-blue-500 hover:bg-blue-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">Get Started</a>
 
                 <a href="https://github.com/gopayee/cashier-usage" title="Jigsaw by Tighten" class="bg-gray-400 hover:bg-gray-600 text-blue-900 font-normal hover:text-white rounded py-2 px-6">Repo</a>
             </div>
